d9a7a1a0bd7d40de66f73050a18a4579dbb9c452
EmirhanKisa <kisaemirhan57@gmail.com>
2025-07-15 13:36:01 +0300

    Add files via upload

---
+++ LeetCode Problem 1502.py
@@ -0,0 +1,14 @@
+liste = [4, 8, 6, 2]
+
+liste.sort()
+s = liste[1] - liste[0]  # Aritmetik fark
+
+for i in range(len(liste) - 1):
+
+    if s != liste[i + 1] - liste[i]:
+
+        print("Bu liste aritmatik değil")
+        break
+
+else:
+    print("Bu liste aritmatik")
