6944fddbf1d019b2e6c16c917f475ed8912382fa
EmirhanKisa <kisaemirhan57@gmail.com>
2025-07-17 13:18:09 +0300

    Add files via upload

---
+++ LeetCode Problem 1.py
@@ -0,0 +1,13 @@
+nums = [2, 7, 11, 15]
+target = 9
+
+for i in range(len(nums)):
+
+    for j in range(i + 1, len(nums)):
+
+        if i != j:
+            if nums[i] + nums[j] == target:
+                print(f"{i}. ve {j}. indexler ile bulunuyor")
+
+
+
